527ee80def38b0196b336b9a56f047e788871efc
elen-oz <ozerova.lenka@gmail.com>
2024-01-22 11:19:16 +0100

    refactor: replace useState by Redux Toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment, incrementByAmount } from './redux/counter';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='card'>
+      <h2>count is {count}</h2>
+
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>increment</button>
+      <br />
+      <button onClick={() => dispatch(incrementByAmount(33))}>
+        increment by 33
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
+import App from './App.jsx';
+import store from './redux/store.js';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
